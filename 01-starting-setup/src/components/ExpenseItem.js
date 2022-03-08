4976f989a599c4ed422cae3b02dc0f884839019f
David Melsheimer <dave.melshman@gmail.com>
2022-03-07 23:55:06 -0500

    commit

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 28th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <h2>$294.67</h2>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
