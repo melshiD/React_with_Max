3409c5daad6fd87b230bdc2028137d58fb40e0c3
David Melsheimer <dave.melshman@gmail.com>
2022-03-10 13:34:13 -0500

    added dismiss functionality to error modal

--- practice-project-1/01-starting-project/src/components/Users/AddUser.js
+++ practice-project-1/01-starting-project/src/components/Users/AddUser.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
+
     const addUserHandler = (event) => {
-        event.preventDefauilt();
+        event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter a valid name and age'
+            });
+            return;
+        }
+        if(+enteredAge < 1){
+            setError({
+                title: 'Invalid Age',
+                message: 'Please enter a valid age input (number > 0)'
+            });
+        }
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUserName('');
+        setEnteredAge('');
+    }
+    const usernameChangeHandler = event => {
+        setEnteredUserName(event.target.value);
+    }
+    const ageChangeHandler = event => {
+        setEnteredAge(event.target.value);
     }
 
-    return(
-    <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input type="text" id='username'/>
-        <label htmlFor='age'>Age (Years)</label>
-        <input type="number" id='age'/>
-        <button type='submit'>Add User</button>
-    </form>
+    const onDismissModal = () => {
+        return setError(false);
+    }
+
+    return (
+        <div>
+            {error && <ErrorModal 
+                        title={error.title} 
+                        message={error.message} 
+                        dismissModal={onDismissModal}
+                      />
+            }
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor='username'>Username</label>
+                    <input type="text" id='username' value={enteredUsername} onChange={usernameChangeHandler} />
+                    <label htmlFor='age'>Age (Years)</label>
+                    <input type="number" id='age' value={enteredAge} onChange={ageChangeHandler} />
+                    <Button type='submit'>Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 };
 
