0b79cb0c8002b6fec9917254b40dbdcf3f1e57b4
David Melsheimer <dave.melshman@gmail.com>
2022-03-11 17:15:16 -0500

    filling in meal item styles

--- practice-project-food-app/src/App.js
+++ practice-project-food-app/src/App.js
@@ -1,8 +1,15 @@
+import Header from "./Components/Layout/Header";
+import React, {Fragment} from 'react';
+import Meals from "./Components/Meals/Meals";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
