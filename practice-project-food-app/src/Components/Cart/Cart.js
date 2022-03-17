37914992a486506468681a656dd6a1cf73618cee
David Melsheimer <dave.melshman@gmail.com>
2022-03-17 09:03:02 -0400

    adding state to cart.  Why is total amount in cart NaN

--- practice-project-food-app/src/Components/Cart/Cart.js
+++ practice-project-food-app/src/Components/Cart/Cart.js
@@ -1,20 +1,28 @@
+import {useContext} from 'react';
+import CartContext from '../../store/cart-context';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 
 const Cart = props => {
-    const cartItems = <ul className={classes['card-item']}>{[{id: 'c1', 
-                        name: 'Sushi', 
-                        amount: 2, 
-                        price: 12.99}].map( item => {
-                            return <li>{item.name}</li>
-                        })}</ul>
+    const cartCtx = useContext(CartContext);
+console.log(cartCtx);
+console.log(cartCtx.totalAmount.toFixed(2));
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItems = <ul 
+        className={classes['card-item']}>{
+        cartCtx.items.map( item => {
+            return <li>{item.name}</li>
+        })}
+    </ul>;
 
     return(
         <Modal hideCart={props.hideCart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button 
@@ -22,7 +30,7 @@ const Cart = props => {
                     onClick={props.hideCart}
                     >Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
