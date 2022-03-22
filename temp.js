fad33668f0502b9ab384c56b1fac3fffea3e6352
David Melsheimer <dave.melshman@gmail.com>
2022-03-22 12:10:07 -0400

    untangling git

--- temp.js
+++ temp.js
@@ -1,3 +1,21 @@
+import React from 'react';
+
+import classes from './Movie.module.css';
+
+const Movie = (props) => {
+  return (
+    <li className={classes.movie}>
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+    </li>
+  );
+};
+
+export default Movie;
+
+
+
 import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
