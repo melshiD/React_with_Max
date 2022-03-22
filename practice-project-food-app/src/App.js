dee6658a1dbfa73347077f1eab1e743a45356f7c
David Melsheimer <dave.melshman@gmail.com>
2022-03-22 12:18:15 -0400

    re-added Star Wars api example to project

--- practice-project-food-app/src/App.js
+++ practice-project-food-app/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
