a204063b54b57e2d79f2c80a5657766ada234a08
David Melsheimer <dave.melshman@gmail.com>
2022-04-22 09:06:14 -0400

    awesome form validation

--- practice-project-food-app/src/Components/Cart/Checkout.js
+++ practice-project-food-app/src/Components/Cart/Checkout.js
@@ -1,27 +1,76 @@
 import classes from './Checkout.module.css';
+import {useRef, useState} from 'react';
+
+const isEmpty = value => {
+    return value.trim() === '';
+}
+const isNotFiveChars = value => value.trim().length !== 5;
 
 const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+      name: true,
+      street: true,
+      city: true,
+      postal: true
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalIsValid = !isNotFiveChars(enteredPostal);
+
+    setFormInputsValidity({
+        name: enteredNameIsValid,
+        street: enteredStreetIsValid,
+        city: enteredCityIsValid,
+        postal: enteredPostalIsValid
+    });
+
+    const formIsValid = enteredNameIsValid && enteredStreetIsValid && enteredCityIsValid && enteredPostalIsValid;
+
+    if(!formIsValid){
+        return;
+    }
+    //submit cart data
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${!formInputsValidity.name?classes.invalid:''}`}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' ref={nameInputRef}/>
+        {!formInputsValidity.name && <p>Please enter a valid name</p>}
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${!formInputsValidity.street?classes.invalid:''}`}>
         <label htmlFor='street'>Street</label>
-        <input type='text' id='street' />
+        <input type='text' id='street' ref={streetInputRef}/>
+        {!formInputsValidity.street && <p>Please enter a valid street address</p>}
+
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${!formInputsValidity.postal?classes.invalid:''}`}>
         <label htmlFor='postal'>Postal Code</label>
-        <input type='text' id='postal' />
+        <input type='text' id='postal' ref={postalInputRef}/>
+        {!formInputsValidity.postal && <p>Please enter a valid zip code (5 chars)</p>}
+
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${!formInputsValidity.city?classes.invalid:''}`}>
         <label htmlFor='city'>City</label>
-        <input type='text' id='city' />
+        <input type='text' id='city' ref={cityInputRef}/>
+        {!formInputsValidity.city && <p>Please enter a valid city name</p>}
+
       </div>
       <div className={classes.actions}>
         <button type='button' onClick={props.hideCart}>
