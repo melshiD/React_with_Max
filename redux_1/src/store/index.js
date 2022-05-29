11efd9adeaa0c13b5dbe2bf086c04d66c0a53dfb
David Melsheimer <dave.melshman@gmail.com>
2022-05-28 23:05:04 -0400

    redux toolkit and state slices

--- redux_1/src/store/index.js
+++ redux_1/src/store/index.js
@@ -1,19 +1,49 @@
 import {createStore} from 'redux';
+import {createSlice} from '@reduxjs/toolkit'
+const initialState = {counter: 0, showCounter: true};
 
-const counterReducer = (state = {counter: 0, facePaint: 'purple'}, action) => {
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter ++;
+        },
+        decrement(state) {
+            state.counter --;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment'){
         return{
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
     if(action.type === 'decrement'){
         return{
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
         }
     }
-    if(action.type === 'changeColor'){
+    if(action.type === 'increase'){
         return{
-            facePaint: 'green'
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
+    }
+    if(action.type === 'toggle'){
+        return{
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
     return state;
