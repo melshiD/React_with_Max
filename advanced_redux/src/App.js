eb4c5508606924a8d50d575ef38ffca7424957d6
David Melsheimer <dave.melshman@gmail.com>
2022-06-27 22:28:15 -0400

    action creator... thunk!

--- advanced_redux/src/App.js
+++ advanced_redux/src/App.js
@@ -1,23 +1,78 @@
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Fragment, useEffect, useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 import uiSlice from './store/ui-slice';
+import { uiActions } from './store/ui-slice';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cartState = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
   useEffect(() => {
-    fetch('https://react-http-b3bb7-default-rtdb.firebaseio.com/cart.json', 
-            {method: 'PUT', 
-            body: JSON.stringify(cartState)});
-  }, [cartState]);
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'sending request',
+        message: 'sending cart data'
+      }));
+
+      //WHEN YOU SIT BACK DOWN, LEARN ABOUT ACTION CREATOR THUNKS
+
+      const response = await fetch('https://react-http-b3bb7-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartState)
+        });
+
+      if (!response.ok) {
+        throw new Error('sending cart data failed')
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'sent cart data successfully'
+      })
+      );
+    }
+
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'sending cart data failed'
+      }));
+    });
+
+  }, [cartState, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && 
+        (<Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+
   );
 }
 
--- advanced_redux/src/store/ui-slice.js
+++ advanced_redux/src/store/ui-slice.js
@@ -2,12 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartIsVisible: false },
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggle(state){
             state.cartIsVisible = !state.cartIsVisible;
             //not actually mutating state here because we're using
             //toolkit
+        },
+        showNotification(state, action){
+            state.notification = { status: action.payload.status, 
+                                    title: action.payload.title, 
+                                    message: action.payload.message};
         }
     }
 });
