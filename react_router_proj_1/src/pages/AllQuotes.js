d3e1f3f6f3ce76a61aac69d6ed94248ef4079d39
David Melsheimer <dave.melshman@gmail.com>
2022-06-30 10:51:00 -0400

    more React router.  Preeeety cool

--- react_router_proj_1/src/pages/AllQuotes.js
+++ react_router_proj_1/src/pages/AllQuotes.js
@@ -1,5 +1,15 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_DATA = [
+    {id: 'q1', author: 'Winston Churchill', text: 'Success if not final; failure is not fatal:'},
+    {id: 'q2', author: 'David Melsheimer', text: 'Sidevoid Crack!'},
+    {id: 'q3', author: 'Maximilian', text: 'learning react is fun'},
+    {id: 'q4', author: 'Max', text: 'Learning react is great!'},
+    {id: 'q5', author: 'Winston Churchill', text: 'We will fight them...'},
+]
+
 const AllQuotes = () => {
-    return <h1>All Quotes</h1>
+    return <QuoteList quotes={DUMMY_DATA}/>
 };
 
 export default AllQuotes;
