d2acae1e3e0eaafe7044e8bcca27711fa015d448
David Melsheimer <dave.melshman@gmail.com>
2022-06-30 21:13:25 -0400

    sessions and auth with Firebase as dummy backend

---
+++ authentication/src/components/Layout/MainNavigation.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+
+
+//WHEN YOU SIT BACK DOWN, BEGIN WITH THE NEXT LECTURE VIDEO FOR AUTHENTICATION
+
+
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
