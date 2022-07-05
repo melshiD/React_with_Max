a1d8c55ce6190c79ad5ee09b469e896ca4acbfc1
David Melsheimer <dave.melshman@gmail.com>
2022-07-05 19:07:13 -0400

    making requests to atlas db

--- nextjs_practice_proj/pages/api/new-meetup.js
+++ nextjs_practice_proj/pages/api/new-meetup.js
@@ -1,12 +1,25 @@
+import { MongoClient } from 'mongodb';
+import credentials from "../../credentials.js";
+
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
+    const {password, username} = credentials();
     if(req.method === "POST"){
+        console.log(req.body);
+        // const data = JSON.parse(req.body);
         const data = req.body;
 
-        const {title, image, address, description} = data;
+        const client = await MongoClient.connect(`mongodb+srv://${username}:${password}@cluster0.fjg9ckv.mongodb.net/meetups?retryWrites=true&w=majority`);
+        const db = client.db('meetups');
 
-        
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+
+        console.log(result);
+        client.close();
+
+        res.status(201).json({message: 'Meeupt inserted'});
     }
 }
 
